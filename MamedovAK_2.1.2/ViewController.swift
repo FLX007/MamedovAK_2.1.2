89efe27c2fa0f808f4457bd0fe2fe0b2cfb44b59
FKX007 <aamiran3007@yandex.ru>
2021-10-20 00:58:52 +0300

    Initial Commit

---
+++ MamedovAK_2.1.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MamedovAK_2.1.2
+//
+//  Created by FR on 20.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
